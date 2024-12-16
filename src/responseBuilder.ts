bae4211c1bb4c474f099308c11bc44e4c01748d8
Manon <manon.jacquin@gmail.com>
2024-12-16 12:34:16 -0800

    feat: build HTTP/1.1 response and return formatted response

---
+++ src/responseBuilder.ts
@@ -0,0 +1,32 @@
+/* 
+    Once we have a parsed request, we can build the response.
+    We therefore need to build a valid HTTP response string.
+*/
+
+export interface HttpResponseOptions {
+    statusCode: number;
+    statusMessage?: string;
+    headers?: Record<string, string>;
+    body?: string;
+}
+
+export function buildHttpResponse(options: HttpResponseOptions): string {
+    const {
+        statusCode = 200,
+        statusMessage = 'OK',
+        headers = {},
+        body = '',
+    } = options
+
+
+    const headerLines = Object.entries(headers)
+    .map(([key, value]) => `${key}: ${value}`)
+    .join('\r\n');
+
+    console.log("HEADERLINES", headerLines)
+  // Note: \r\n separates lines in HTTP
+    return `HTTP/1.1 ${statusCode} ${statusMessage}\r\n` +
+        `${headerLines}\r\n` +
+        `\r\n` +
+        `${body}`;
+}
