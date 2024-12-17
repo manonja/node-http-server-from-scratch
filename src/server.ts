8a095334a6e35e316a57145439343eefa546f354
Manon <manon.jacquin@gmail.com>
2024-12-17 09:41:05 -0800

    feat: make necessary changes on server for dynamic request handler

--- src/server.ts
+++ src/server.ts
@@ -4,67 +4,10 @@
 */
 
 import net from 'node:net';
-import { parseRequest } from './requestParser';
-import { buildHttpResponse } from './responseBuilder';
+import { handleRequest } from './handleRequest';
 
 
-const handleRequest = (requestString: string) => {
-    const request = parseRequest(requestString);
-    if (!request) {
-        // The request is malformed; return 400
-        return buildHttpResponse({
-            statusCode: 400,
-            statusMessage: 'Bad Request',
-            headers: {
-                'Content-Type': 'text/plain',
-                'Content-Length': '11'
-            },
-            body: 'Bad Request'
-        });
-    }
-
-    // If the request is valid, route based on the path
-    if (request.path === '/') {
-        // Return Home
-        const responseBody = 'Welcome to the Home Page!';
-        return buildHttpResponse({
-            statusCode: 200,
-            statusMessage: 'OK',
-            headers: {
-                'Content-Type': 'text/plain',
-                'Content-Length': responseBody.length.toString()
-            },
-            body: responseBody
-        });
-    } else if (request.path === '/api') {
-        // Return some JSON response
-        const responseData = { message: "Hello from the API" };
-        const responseBody = JSON.stringify(responseData);
-        return buildHttpResponse({
-            statusCode: 200,
-            statusMessage: 'OK',
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': responseBody.length.toString()
-            },
-            body: responseBody
-        });
-    } else {
-        // Return 404 for other routes
-        const responseBody = 'Not Found';
-        return buildHttpResponse({
-            statusCode: 404,
-            statusMessage: 'Not Found',
-            headers: {
-                'Content-Type': 'text/plain',
-                'Content-Length': responseBody.length.toString()
-            },
-            body: responseBody
-        });
-    }
-}
-
-const createDataHandler = (socket: net.Socket, buffer: string): ((chunk: Buffer) => void) => {
+const createDataHandler = (socket: net.Socket, buffer: string, handleRequest: (requestString: string) => string): ((chunk: Buffer) => void) => {
     return (chunk: Buffer) => {
         const newBuffer = buffer.concat(chunk.toString());
         // Check if we have a full HTTP request, headers ends with \r\n\r\n
@@ -75,14 +18,14 @@ const createDataHandler = (socket: net.Socket, buffer: string): ((chunk: Buffer)
         } else {
             // Not enough data to parse a request. Replace the current listener with one that remembers the latest state of the buffer
             socket.removeAllListeners('data');
-            socket.on('data', createDataHandler(socket, newBuffer));
+            socket.on('data', createDataHandler(socket, newBuffer, handleRequest));
         }
     }
 }
 
 const server = net.createServer((socket) => {
     // Start with an empty buffer
-    socket.on('data', createDataHandler(socket, ''));
+    socket.on('data', createDataHandler(socket, '', handleRequest));
 });
 
 server.listen(8080, () => {
